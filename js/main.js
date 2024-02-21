551daee02197a899d32095466761a98f5870992e
riskiestbiscut <sa9smith@uwaterloo.ca>
2024-02-20 20:09:44 -0500

    add functionality for clicks
    add use of enter key for initial search.
    add event to click on drink img

--- js/main.js
+++ js/main.js
@@ -1,33 +1,65 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// submit cocktail choice on click
 document.querySelector('.butt').addEventListener('click', getDrink)
+
+// submit cocktail choice on enter key
+document.querySelector('.textInput').addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.querySelector(".butt").click();
+    }
+  });
+
+ function setSelectors() {
+    let drinkItem = document.querySelectorAll('img')
+
+    for (let i=0; i < drinkItem.length; i++) {
+      drinkItem[i].addEventListener('click', openDrinkItem)
+    }
+ }
 
 function getDrink() {
     let drink = document.querySelector('input').value
     let url ="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
+    
 
 fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
         console.log(data)
+        //loop through queried drink array
         for (let i=0; i < data.drinks.length; i++) {
+            //create needed DOM elements
             let li = document.createElement("li");
             let img = document.createElement("img")
             let p = document.createElement("p")
+            //attach content to DOM elements
             img.src = data.drinks[i].strDrinkThumb
             p.textContent = data.drinks[i].strDrink
 
-            console.log(li, img, p)
+            //append elements in DOM
             li.appendChild(img);
-            console.log(li)
             li.appendChild(p);
-            console.log(li)
             document.querySelector("ul").appendChild(li);
 
+            setSelectors()
         }
-                // console.log(data.drinks)
-                // document.querySelector('h2').innerHTML = data.drinks[i].strDrink
-                // document.querySelector('img').src = data.drinks[i].strDrinkThumb
-                // document.querySelector('h3').innerHTML = data.drinks[i].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+   
+}
+
+
+function openDrinkItem(e) {
+    let drink = e.target.textContent
+    let url ="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drink
+    console.log("clicked")
+    fetch(url)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
     })
     .catch(err => {
         console.log(`error ${err}`)
